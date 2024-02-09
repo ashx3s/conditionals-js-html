43868ac0953613b4ce8e26d6675c7ab26e991de2
Ashlyn Knox <ashlyn@lilyx.io>
2024-02-08 19:13:35 -0700

    code cleanup

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 let firstString = "this is our first string. yay!";
 let firstNumber = 42;
 
@@ -6,23 +7,15 @@ let firstNumber = 42;
 const strElement = document.querySelector("#first-string");
 const numElement = document.querySelector("#first-number");
 
-// user form examples
-const userNameEl = document.querySelector("#user-name");
-const userIdEl = document.querySelector("#user-id");
-const userForm = document.querySelector("#user-form");
-
-/* 
-ways to add information to html:
-1. textContent == only text, not style aware
-2. innerText == style aware and more powerful but more expensive
-3. innerHTML == most powerful, most insecure, parses actual html elements
-
-*/
-
 strElement.textContent = firstString;
 numElement.textContent = "the meaning of life is" + " " + firstNumber;
 
 // user examples
+
+// user form examples
+const userNameEl = document.querySelector("#user-name");
+const userIdEl = document.querySelector("#user-id");
+const userForm = document.querySelector("#user-form");
 
 let userName;
 
@@ -51,6 +44,7 @@ function handleSubmit(event) {
 userForm.addEventListener("submit", handleSubmit);
 
 // Calculator Code
+
 const calculator = document.querySelector("#calculator");
 const calculatorOutput = document.querySelector("#output");
 
